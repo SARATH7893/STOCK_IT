03d65a8425f395401adc9abcdb3a963323f8e64e
SARATH7893 <kise110604@gmail.com>
2024-07-27 18:16:21 +0530

    dashboard-routes

---
+++ frontend/src/landing_page/OpenAccount.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function OpenAccount() {
+    return ( 
+    <div className='container p-5 mb-5'>
+        <div className='row text-center'>
+                <h1 className='mt-5'> Open A StockIt Account</h1>
+                <p>Modern Platforms And Apps , ₹0 Investments, And Flat ₹20 Intraday And F&O Trades.</p>
+                <button className='p-2 btn btn-primary fs-5 mb-5' style={{width:"20%",margin:"0 auto"}}>Sign Up Now</button>
+        </div>
+    </div>
+    );
+}
+
+export default OpenAccount;
---
+++ frontend/src/landing_page/products/Universe.js
@@ -0,0 +1,48 @@
+import React from "react";
+
+function Universe() {
+  return (
+    <div className="container mt-5">
+      <div className="row text-center">
+        <h1>The StockIt Universe</h1>
+        <p>
+          Extend Your Trading And Investment Experience Even Further With Our
+          Partner Platforms
+        </p>
+
+        <div className="col-4 p-3 mt-5">
+          <img src="media/images/smallcaseLogo.png" />
+          <p className="text-small text-muted">Thematic Investment Platform</p>
+        </div>
+        <div className="col-4 p-3 mt-5">
+          <img src="media/images/streakLogo.png" style={{width:"35%"}}/>
+          <p className="text-small text-muted">Algo & Strategy Platform</p>
+        </div>
+        <div className="col-4 p-3 mt-5">
+          <img src="media/images/sensibullLogo.svg" style={{width:"40%"}}/>
+          <p className="text-small text-muted">Options Trading Platform</p>
+        </div>
+        <div className="col-4 p-3 mt-5">
+          <img src="media/images/Screenshot(274).png " style={{width:"35%"}} />
+          <p className="text-small text-muted">Asset Management</p>
+        </div>
+        <div className="col-4 p-3 mt-5">
+          <img src="media/images/goldenpilogo.png" />
+          <p className="text-small text-muted">Bonds Trading Platform</p>
+        </div>
+        <div className="col-4 p-3 mt-5">
+          <img src="media/images/dittoLogo.png" style={{width:"35%"}}/>
+          <p className="text-small text-muted">Insurance</p>
+        </div>
+        <button
+          className="p-2 btn btn-primary fs-5 mb-5"
+          style={{ width: "20%", margin: "0 auto" }}
+        >
+          Signup Now
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Universe;
